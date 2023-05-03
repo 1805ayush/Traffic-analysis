5632345341a305f34193a184d5d2ada05df0290d
1805ayush <ayush.chakladar@gmail.com>
2023-05-03 17:37:00 +0530

    flask app

---
+++ web/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, url_for
+import pickle
+import numpy as np 
+from datetime import datetime
+
+app = Flask(__name__)
+
+model = pickle.load(open('saved_model.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('test.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+
+    # print("AA")
+    int_features = request.form.get('Date')
+    newDate = datetime.strptime(int_features, '%Y-%m-%dT%H:%M')
+
+    print(newDate)
+    print(newDate.day)
+    
+    future = model.make_future_dataframe(periods=24000 ,freq='h')
+    forecast = model.predict(future)
+    date = datetime(newDate.year, newDate.month, newDate.day, newDate.hour, 0, 0)
+    
+    var =0
+    if date>datetime(2017,5, 14,0,0,0):
+        return render_template('test.html', prediction_text='The number of vehicles predicted is has not been predicted yet.')
+    else:
+        var = forecast.loc[forecast['ds']==date,'yhat'].values[0]
+        var = var.round()
+    
+
+    return render_template('test.html', prediction_text='The number of vehicles predicted is {}'.format(var))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
